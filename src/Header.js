7d7c9776bb7002abcb48b898819f8c48a568eccf
kaiting0407 <mike26593643@gmail.com>
2024-05-05 02:01:58 +0800

    first

---
+++ src/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li><a href="">HOME</a></li>
+          <li><a href="">GRADE</a></li>
+          <li><a href="">Terms Of Service</a></li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default header
